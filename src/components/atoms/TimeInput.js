1f9068b88a07299ca1cdbf80c4fde553dca085fa
hemant.p <hemant.p@infanion.com>
2023-09-29 11:02:50 +0530

    Added TimeInput atom and time field in add sector page

---
+++ src/components/atoms/TimeInput.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import TimePicker from "../../utilities/TimePicker";
+import { t } from "../../translations/Translation.js";
+import { GET_CONSTANTS } from "../../applicationConstants/AppConstants";
+import RequiredIcon from "../../static/icons/exclamation-mark1.png"
+
+
+export default function TimeInput({ setTime, index, type, value, title, customStyle, required, error, styleMargin }) {
+    const [startPicker, setStartPicker] = useState(false);
+
+    return (
+        <div className={"col-3 title-position font-weight-bold " + customStyle} >
+            {/* {title && <label htmlFor="start_time" onClick={() => hideTimePopup()}  className="filter-title row m-0 mb-1">
+                {t(GET_CONSTANTS.START_TIME)}
+                <p className="text-danger mb-0 font-wieght-bold">{title}&nbsp;*</p>
+            </label>} */}
+            <div className={"d-flex justify-content-between " + (error ? '' : styleMargin)} >
+                <label className="row mx-0 my-auto">{title} {required && <p className="text-danger my-auto">&nbsp;*</p>}</label>
+                {error && <p className="pt-1 pb-0 px-4 m-1 text-danger required-bg rounded font-weight-normal">
+                    <img className="box mr-1 mb-1" src={RequiredIcon}></img>
+                    {error}
+                </p>}
+            </div>
+            {startPicker && (
+                <TimePicker
+                    hour={value ? value.split(':')[0] : '00'}
+                    minute={value ? value.split(':')[1] : '00'}
+                    index={index}
+                    type={type}
+                    setHourMin={setTime}
+                />
+            )}
+            <input
+                type=""
+                name="start_time"
+                onClick={() => setStartPicker(!startPicker)}
+                value={value}
+                onChange={(e) => { setTime(e.target.value, type, index); }}
+                className="form-control m-0"
+            />
+        </div>
+
+    );
+}
