fe7267af94cd592f56cd760133e46c03f7e6a88c
sayeed <sayeed.shaik@infanion.com>
2023-10-31 20:19:58 +0530

    sayeed - Ckeditor changes

---
+++ ckeditor/sample/script.js
@@ -0,0 +1,24 @@
+createDialog().then( config => {
+	return ClassicEditor
+		.create( document.querySelector( '.editor' ), {
+			ckbox: {
+				tokenUrl: config.ckboxTokenUrl
+			}
+		} )
+		.then( editor => {
+			window.editor = editor;
+		} )
+		.catch( handleSampleError );
+} );
+
+function handleSampleError( error ) {
+	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+	const message = [
+		'Oops, something went wrong!',
+		`Please, report the following error on ${ issueUrl } with the build id "nmizac7xww9h-6445eicd1uf8" and the error stack trace:`
+	].join( '\n' );
+
+	console.error( message );
+	console.error( error );
+}
---
+++ ckeditor/src/ckeditor.ts
@@ -0,0 +1,177 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import {
+	Bold,
+	Code,
+	Italic,
+	Subscript,
+	Superscript,
+	Underline
+} from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { CKBox } from '@ckeditor/ckeditor5-ckbox';
+import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
+import {
+	DataFilter,
+	DataSchema,
+	GeneralHtmlSupport,
+	HtmlComment
+} from '@ckeditor/ckeditor5-html-support';
+import {
+	AutoImage,
+	Image,
+	ImageCaption,
+	ImageInsert,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	PictureEditing
+} from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
+import { PageBreak } from '@ckeditor/ckeditor5-page-break';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { SelectAll } from '@ckeditor/ckeditor5-select-all';
+import { SpecialCharacters } from '@ckeditor/ckeditor5-special-characters';
+import { Style } from '@ckeditor/ckeditor5-style';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Alignment,
+		AutoImage,
+		AutoLink,
+		Autoformat,
+		BlockQuote,
+		Bold,
+		CKBox,
+		CloudServices,
+		Code,
+		CodeBlock,
+		DataFilter,
+		DataSchema,
+		Essentials,
+		FindAndReplace,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
+		GeneralHtmlSupport,
+		Heading,
+		Highlight,
+		HorizontalLine,
+		HtmlComment,
+		HtmlEmbed,
+		Image,
+		ImageCaption,
+		ImageInsert,
+		ImageResize,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		LinkImage,
+		List,
+		MediaEmbed,
+		MediaEmbedToolbar,
+		PageBreak,
+		Paragraph,
+		PasteFromOffice,
+		PictureEditing,
+		SelectAll,
+		SpecialCharacters,
+		Style,
+		Subscript,
+		Superscript,
+		Table,
+		TableToolbar,
+		TextTransformation,
+		Underline
+	];
+
+	public static override defaultConfig = {
+		toolbar: {
+			items: [
+				'undo',
+				'redo',
+				'heading',
+				'style',
+				'|',
+				'ckbox',
+				'bold',
+				'italic',
+				'underline',
+				'subscript',
+				'superscript',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'outdent',
+				'indent',
+				'|',
+				'imageUpload',
+				'blockQuote',
+				'insertTable',
+				'mediaEmbed',
+				'alignment',
+				'codeBlock',
+				'fontBackgroundColor',
+				'findAndReplace',
+				'fontColor',
+				'fontSize',
+				'fontFamily',
+				'highlight',
+				'imageInsert',
+				'selectAll',
+				'specialCharacters'
+			]
+		},
+		language: 'en-gb',
+		image: {
+			toolbar: [
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'imageStyle:inline',
+				'imageStyle:block',
+				'imageStyle:side',
+				'linkImage'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells'
+			]
+		}
+	};
+}
+
+export default Editor;
