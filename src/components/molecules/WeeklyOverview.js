d7bd2bca7f97a0aad790f0dd8ccc9fb99e1295ac
hemant.p <hemant.p@infanion.com>
2024-02-01 20:03:54 +0530

    added activate dashboard button in location table

--- src/components/molecules/WeeklyOverview.js
+++ src/components/molecules/WeeklyOverview.js
@@ -463,13 +463,8 @@ export default function WeeklyOverview({ enableShifts, weekNumber, year, locId, 
                                                 </div>
                                             </td>}
                                             {/* Employee and plan data rows */}
-<<<<<<< HEAD
-                                            <td>{ws_employee.employee_id ? <a className="text-dark text-truncate plannign_overview_weekly_employee_title" href={"/manage-employees/" + ws_employee.employee_id} title={ws_employee.employee_name}>{ws_employee.employee_name}</a> : ws_employee.employee_name}
-                                                <div> {ws_employee.employee_id && ws_employee.employee_types !==null &&ws_employee.employee_types !== undefined &&
-=======
                                             {/* <td>{ws_employee.employee_id ? <a className="text-dark text-truncate plannign_overview_weekly_employee_title" href={"/manage-employees/" + ws_employee.employee_id} title={ws_employee.employee_name}>{ws_employee.employee_name}</a> : ws_employee.employee_name}
                                                 <div> {ws_employee.employee_id && ws_employee.employee_types !== null && ws_employee.employee_types !== undefined && Object.keys(ws_employee.employee_types).length !== 0 &&
->>>>>>> d5665efe42572861b36504e59fe905c984f7652c
                                                     //mapping employeetype and its colour
                                                     Object.keys(ws_employee.employee_types).map((key, index) => {
                                                         return (
