8326f0c6d3ce3b3bb18e1ee32ffa62ed90b84f70
laxmi <laxmiparwati@infanion.com>
2024-02-05 11:33:38 +0530

    Added open uurrooster

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,7 @@ import IndiiBanner from "../static/icons/indii.jpeg";
 import { APICALL as AXIOS } from "../services/AxiosServices";
 import { LoginApiUrl } from "../routes/ApiEndPoints";
 import Dropdown from "../components/atoms/Dropdown";
+import Uurrooster from "./Uurrooster";
 
 export default function Login({ setAuth }) {
 
@@ -16,7 +17,7 @@ export default function Login({ setAuth }) {
     const [password, setPassword] = useState('');
 
     const [message, setMessage] = useState(t("FORGOT_PASSWORD_INSTRUCTION"));
-    const [activeLanguage, setActiveLanguage] = useState({ value: localStorage.getItem('active_language')==null?"nl": localStorage.getItem('active_language'), label: (localStorage.getItem('active_language')==null?"nl":localStorage.getItem('active_language'))?.toUpperCase() })
+    const [activeLanguage, setActiveLanguage] = useState({ value: localStorage.getItem('active_language') == null ? "nl" : localStorage.getItem('active_language'), label: (localStorage.getItem('active_language') == null ? "nl" : localStorage.getItem('active_language'))?.toUpperCase() })
     //Language options
     const LanguageOptions = [
         { value: 'en', label: 'EN' },
@@ -68,62 +69,79 @@ export default function Login({ setAuth }) {
     localStorage.setItem('auth', false);
 
     return (
-        <div className="col-md-12 d-flex full-page-height align-items-center p-0">
-            <div className="col-md-6 p-0 border-right full-page-height">
-                <img className="banner-style" src={IndiiBanner}></img>
-            </div>
-            {/* <div className="col-md-5 p-0 full-page-height"> */}
-            <div className="col-md-5 mx-auto d-flex align-items-center justify-content-center float-right position-relative h-100 ">
-                <div className="position-absolute login_lang_dropdown mt-3">
-                    <ul className="navbar-nav">
-                        <li className="mx-3 px-2 w-max-content">
-                            <Dropdown
-                                key={"login_page"}
-                                options={LanguageOptions}
-                                selectedOptions={activeLanguage}
-                                onSelectFunction={(e) => { setActiveLanguage(e); localStorage.setItem('active_language', e?.value); window.location.reload(); }}
-                                styleClass="language-dropdown"
-                            ></Dropdown>
-                        </li>
-                    </ul>
-                </div>
-                <div className="w-75">
-                    <div className="mb-5 text-center">
-                        <img alt={t("LOGO")} className="login-logo" src={Logo}></img>
+        <>
+            {localStorage.getItem('dashboard_access_token') && <nav className="navbar navbar-expand-sm bg-white navbar-light px-4 mx-auto shadow-sm border-bottom py-3 justify-content-between">
+                <div className="d-flex col-xl-3 col-lg-4">
+                    <div className=" align-items-center">
+                        <a className="navbar-brand p-0" href="/"><img alt={t("LOGO")} className="logo" src={Logo}></img></a>
                     </div>
-                    <br></br>
-
-                    {/* LOGIN */}
-                    {window.location.hash === '' && <>
-                        <TextInput title={t('USERNAME')} name={"username"} setValue={setUserName} placeholder={""} CustomStyle={"col-md-8 mx-auto"} required={true}></TextInput>
-                        <br></br>
-                        <TextInput title={t('PASSWORD')} name={"password"} setValue={setPassword} placeholder={""} CustomStyle={"col-md-8 mx-auto"} required={true}></TextInput>
-                        <p className="mt-3 font-weight-bold text-right col-md-8 mx-auto" >
-                            <a className="text-color" href="#forgot-password">{t("FORGOT_YOUR_PASSWORD")}?</a>
-                        </p>
-                        <div className="col-md-8 mx-auto">
-                            <CustomButton buttonName={t("LOGIN")} ActionFunction={() => Authenticate()} CustomStyle={"col-md-12 mx-auto"}></CustomButton>
-                        </div>
-                    </>}
-
-                    {/* FORGOT PASSWORD */}
-                    {window.location.hash === '#forgot-password' && <>
-                        <div className="mb-3 text-center">
-                            <h5><span id="text-indii-blue">{message}</span></h5>
-                        </div>
-                        <br></br>
-                        <TextInput title={t('USERNAME')} name={"username"} setValue={setUserName} placeholder={""} CustomStyle={"col-md-8 mx-auto"} required={true}></TextInput>
-                        <br></br>
-                        <div className="col-md-8 mx-auto pt-3 d-flex justify-content-end">
-                            <CustomButton buttonName={t("GET_LINK")} ActionFunction={() => getLink()} CustomStyle={"col-md-3"}></CustomButton>
-                            <CustomButton buttonName={t("BACK_LINK")} ActionFunction={() => navigate('/login')} CustomStyle={"col-md-3"}></CustomButton>
-                        </div>
-                    </>}
                 </div>
+                <Dropdown
+                    key={"login_page"}
+                    options={LanguageOptions}
+                    selectedOptions={activeLanguage}
+                    onSelectFunction={(e) => { setActiveLanguage(e); localStorage.setItem('active_language', e?.value); window.location.reload(); }}
+                    styleClass="language-dropdown"
+                ></Dropdown>
+            </nav>}
+            <div className="col-md-12 d-flex full-page-height align-items-center p-0">
+                {localStorage.getItem('dashboard_access_token') === '' && <div className="col-md-6 p-0 border-right full-page-height">
+                    <img className="banner-style" src={IndiiBanner}></img>
+                </div>}
+                {localStorage.getItem('dashboard_access_token') && <div className="col-md-9"><Uurrooster view="login"></Uurrooster></div>}
+                {/* <div className="col-md-5 p-0 full-page-height"> */}
+                <div className={"mx-auto d-flex align-items-center justify-content-center float-right position-relative h-100 " + (localStorage.getItem('dashboard_access_token') ? 'col-md-3 p-0 border-left' : 'col-md-5')}>
+                    {localStorage.getItem("dashboard_access_token") === '' && <div className="position-absolute login_lang_dropdown mt-3">
+                        <ul className="navbar-nav">
+                            <li className="mx-3 px-2 w-max-content">
+                                <Dropdown
+                                    key={"login_page"}
+                                    options={LanguageOptions}
+                                    selectedOptions={activeLanguage}
+                                    onSelectFunction={(e) => { setActiveLanguage(e); localStorage.setItem('active_language', e?.value); window.location.reload(); }}
+                                    styleClass="language-dropdown"
+                                ></Dropdown>
+                            </li>
+                        </ul>
+                    </div>}
+                    <div className={localStorage.getItem('dashboard_access_token') ? "w-100" : "w-75"}>
+                        <div className="mb-5 text-center">
+                            <img alt={t("LOGO")} className="login-logo" src={Logo}></img>
+                        </div>
+                        <br></br>
 
-                {/* RESET PASSWORD */}
+                        {/* LOGIN */}
+                        {window.location.hash === '' && <>
+                            <TextInput title={t('USERNAME')} name={"username"} setValue={setUserName} placeholder={""} CustomStyle={"col-md-8 mx-auto"} required={true}></TextInput>
+                            <br></br>
+                            <TextInput title={t('PASSWORD')} name={"password"} setValue={setPassword} placeholder={""} CustomStyle={"col-md-8 mx-auto"} required={true}></TextInput>
+                            <p className="mt-3 font-weight-bold text-right col-md-8 mx-auto" >
+                                <a className="text-color" href="#forgot-password">{t("FORGOT_YOUR_PASSWORD")}?</a>
+                            </p>
+                            <div className="col-md-8 mx-auto">
+                                <CustomButton buttonName={t("LOGIN")} ActionFunction={() => Authenticate()} CustomStyle={"col-md-12 mx-auto"}></CustomButton>
+                            </div>
+                        </>}
+
+                        {/* FORGOT PASSWORD */}
+                        {window.location.hash === '#forgot-password' && <>
+                            <div className="mb-3 text-center">
+                                <h5><span id="text-indii-blue">{message}</span></h5>
+                            </div>
+                            <br></br>
+                            <TextInput title={t('USERNAME')} name={"username"} setValue={setUserName} placeholder={""} CustomStyle={"col-md-8 mx-auto"} required={true}></TextInput>
+                            <br></br>
+                            <div className="col-md-8 mx-auto pt-3 d-flex justify-content-end">
+                                <CustomButton buttonName={t("GET_LINK")} ActionFunction={() => getLink()} CustomStyle={"col-md-3"}></CustomButton>
+                                <CustomButton buttonName={t("BACK_LINK")} ActionFunction={() => navigate('/login')} CustomStyle={"col-md-3"}></CustomButton>
+                            </div>
+                        </>}
+                    </div>
+
+                    {/* RESET PASSWORD */}
+                </div>
+                {/* </div> */}
             </div>
-            {/* </div> */}
-        </div>
+        </>
     )
 }
